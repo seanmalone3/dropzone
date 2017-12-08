fa94e4ec7e79246dee518e7a5092695158774178
Stephen Perkins <sperkins@republic.local>
2017-12-08 18:09:52 -0500

    update flask file

--- dropzone.py
+++ dropzone.py
@@ -1,1 +1,16 @@
-"" 
+from flask import Flask, render_template, request, redirect
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
